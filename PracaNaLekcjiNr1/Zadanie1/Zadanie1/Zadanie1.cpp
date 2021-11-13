f9199a2d9fa294f04e77c0cbd4204fcfda15cf5a
Dominik Skąpski <dominiksk47@gmail.com>
2021-11-13 12:55:01 +0100

    Stworzenie funkcji check

--- PracaNaLekcjiNr1/Zadanie1/Zadanie1/Zadanie1.cpp
+++ PracaNaLekcjiNr1/Zadanie1/Zadanie1/Zadanie1.cpp
@@ -1,6 +1,33 @@
 ﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// Funkcja 'check' sprawdza czy podany tekst jest zapisany z małych liter
+bool check(string tekst)
+{	
+	for (int i = 0; i < tekst.length(); i++)
+	{
+		if ((tekst[i] > 96 && tekst[i] < 123) || tekst[i] == 32)
+			continue;
+		else
+			return false;
+
+		return true;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string tekst;
+
+	cout << "Podaj tekst do szyfrowania (Uzyj jedynie malych liter!)\n";
+	while (true)
+	{
+		getline(cin, tekst);
+
+		if (!check(tekst))
+			cout << endl << "Podaj poprawny tekst!" << endl;
+		else
+			break;
+	}
 }
