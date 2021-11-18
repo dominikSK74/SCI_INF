5df870b0cf8472cee5f974736648cdbd22c02cf5
Dominik Skąpski <dominiksk47@gmail.com>
2021-11-18 15:58:51 +0100

    Utworzenie klas

--- PracaNaLekcjiNr1/Zadanie2/Zadanie2/Zadanie2/Zadanie2.cpp
+++ PracaNaLekcjiNr1/Zadanie2/Zadanie2/Zadanie2/Zadanie2.cpp
@@ -16,74 +16,82 @@ bool check(string tekst)
 	}
 }
 
-// Funkcja 'szyfrCezara' szyfruje podany tekst i zwraca zaszyfrowany tekst
-string szyfrCezara(string tekst)
-{
-	int wartosc;
-	cout << "Podaj wartosc przesuniecia (Wieksza od 0 i mniejsza niz 27!)" << endl;
-	while (true)
-	{
-		cin >> wartosc;
-		if (!(wartosc > 0 && wartosc < 27))
-			cout << endl << "Podaj poprawna wartosc!" << endl;
-		else
-			break;
-	}
-
-	for (int i = 0; i < tekst.length(); i++)
-	{
-		if (tekst[i] == 32)
-			continue;
-		else if (tekst[i] + wartosc > 122)
-			tekst[i] = 96 + ((tekst[i] + wartosc) - 122);
-		else
-			tekst[i] = tekst[i] + wartosc;
-	}
-	return tekst;
-}
-
-// Szyfr przestawieniowy
-string szyfrPrzestawieniowy(string tekst)
-{
-	for (int i = 0; i < tekst.length(); i = i + 2)
-		if (i + 2 > tekst.length())
-			break;
-		else
-			swap(tekst[i], tekst[i + 1]);
-	return tekst;
-}
-
-// Zaszyfrowanie tekstu szyfrem cezara oraz szyfrem przestawieniowym
-string obaSzyfry(string tekst)
-{
-	string tmp = szyfrCezara(tekst);
-	tmp = szyfrPrzestawieniowy(tmp);
-	return tmp;
-}
-
-// Odszyfrowanie szyfru cezara
-void odszyfrowanieCezara(string tekst)
-{
-	string tmp;
-	for (int j = 1; j < 27; j++)
-	{
-		tmp = tekst;
-		for (int i = 0; i < tekst.length(); i++)
+class Szyfrowanie {
+	public:
+		// Funkcja 'szyfrCezara' szyfruje podany tekst i zwraca zaszyfrowany tekst
+		string szyfrCezara(string tekst)
 		{
-			if (tekst[i] == 32)
-				continue;
-			else if (tekst[i] - j < 97)
-				tmp[i] = 122 - (96 - (tmp[i] - j));
-			else
-				tmp[i] = tmp[i] - j;
+			int wartosc;
+			cout << "Podaj wartosc przesuniecia (Wieksza od 0 i mniejsza niz 27!)" << endl;
+			while (true)
+			{
+				cin >> wartosc;
+				if (!(wartosc > 0 && wartosc < 27))
+					cout << endl << "Podaj poprawna wartosc!" << endl;
+				else
+					break;
+			}
+
+			for (int i = 0; i < tekst.length(); i++)
+			{
+				if (tekst[i] == 32)
+					continue;
+				else if (tekst[i] + wartosc > 122)
+					tekst[i] = 96 + ((tekst[i] + wartosc) - 122);
+				else
+					tekst[i] = tekst[i] + wartosc;
+			}
+			return tekst;
 		}
-		cout << tmp << endl;
-	}
-}
+
+		// Szyfr przestawieniowy
+		string szyfrPrzestawieniowy(string tekst)
+		{
+			for (int i = 0; i < tekst.length(); i = i + 2)
+				if (i + 2 > tekst.length())
+					break;
+				else
+					swap(tekst[i], tekst[i + 1]);
+			return tekst;
+		}
+
+		// Zaszyfrowanie tekstu szyfrem cezara oraz szyfrem przestawieniowym
+		string obaSzyfry(string tekst)
+		{
+			string tmp = szyfrCezara(tekst);
+			tmp = szyfrPrzestawieniowy(tmp);
+			return tmp;
+		}
+};
+
+class Odszyfrowanie {
+	public:
+		// Odszyfrowanie szyfru cezara
+		void odszyfrowanieCezara(string tekst)
+		{
+			string tmp;
+			for (int j = 1; j < 27; j++)
+			{
+				tmp = tekst;
+				for (int i = 0; i < tekst.length(); i++)
+				{
+					if (tekst[i] == 32)
+						continue;
+					else if (tekst[i] - j < 97)
+						tmp[i] = 122 - (96 - (tmp[i] - j));
+					else
+						tmp[i] = tmp[i] - j;
+				}
+				cout << tmp << endl;
+			}
+		}
+};
 
 int main()
 {
 	string tekst;
+	Szyfrowanie szyfr;
+	Odszyfrowanie odszyfr;
 
 	// Podanie tekstu do szyfrowania
 	cout << "Podaj tekst do szyfrowania (Uzyj jedynie malych liter!)\n";
@@ -112,17 +120,17 @@ int main()
 		switch (akcja)
 		{
 		case 1:
-			tekst = szyfrCezara(tekst);
+			tekst = szyfr.szyfrCezara(tekst);
 			cout << "Zaszyfrowany tekst: " << tekst << endl;
 			break;
 		case 2:
-			cout << "Zaszyfrowany tekst: " << szyfrPrzestawieniowy(tekst) << endl;
+			cout << "Zaszyfrowany tekst: " << szyfr.szyfrPrzestawieniowy(tekst) << endl;
 			break;
 		case 3:
-			cout << obaSzyfry(tekst) << endl;
+			cout << szyfr.obaSzyfry(tekst) << endl;
 			break;
 		case 4:
-			odszyfrowanieCezara(tekst);
+			odszyfr.odszyfrowanieCezara(tekst);
 			break;
 		default:
 			cout << endl << "Podales bledna wartosc!" << endl;
